b6bda899dda42a8b16d40ca1872e324ed8dc4f17
Andreas V <46223359+idna001@users.noreply.github.com>
2024-01-26 21:25:37 +0100

    add calc

--- src/components/TableComponent/TableComponent.js
+++ src/components/TableComponent/TableComponent.js
@@ -9,7 +9,7 @@ const TableComponent = ({ history }) => {
             <tr>
                 <th>Number 1</th>
                 <th>Number 2</th>
-                <th>Sum</th>
+                <th colSpan="3">Calculation</th>
             </tr>
             </thead>
             <tbody>
@@ -17,12 +17,25 @@ const TableComponent = ({ history }) => {
                 <tr key={index}>
                     <td>{entry.number1}</td>
                     <td>{entry.number2}</td>
-                    <td>{entry.sum}</td>
+                    <td>{entry.sum - 1}</td>
+                    <td style={{ backgroundColor: getColor(entry.sum) }}>{entry.sum}</td>
+                    <td>{entry.sum + 1}</td>
                 </tr>
             ))}
             </tbody>
         </table>
     );
+};
+
+// Funktion, um Farbe basierend auf der Summe zu bestimmen
+const getColor = (sum) => {
+    if (sum < 0) {
+        return 'red'; // Setze die Farbe auf Rot, wenn die Summe negativ ist
+    } else if (sum > 0) {
+        return 'green'; // Setze die Farbe auf Grün, wenn die Summe positiv ist
+    } else {
+        return 'black'; // Setze die Farbe auf Schwarz, wenn die Summe null ist
+    }
 };
 
 export default TableComponent;
