bc48423cf08dca309d73ad3ebf7295828a5ba088
Andreas V <46223359+idna001@users.noreply.github.com>
2024-01-26 21:43:46 +0100

    add delete button

--- src/App.js
+++ src/App.js
@@ -8,22 +8,23 @@ const App = () => {
     const [history, setHistory] = useState([]);
 
     useEffect(() => {
-        // Lade den Verlauf aus dem Local Storage, wenn verfügbar
         const storedHistory = JSON.parse(localStorage.getItem('history'));
         if (storedHistory) {
             setHistory(storedHistory);
         }
-    }, []); // Der leere Array bewirkt, dass dieser Effekt nur einmal nach der Montage ausgeführt wird
+    }, []);
 
     const handleFormSubmit = (number1, number2) => {
         const sum = parseFloat(number1) + parseFloat(number2);
         const newEntry = { number1, number2, sum };
-
-        // Füge das neue Element oben in den Verlauf ein
         setHistory([newEntry, ...history]);
 
-        // Speichere den aktualisierten Verlauf im Local Storage
         localStorage.setItem('history', JSON.stringify([newEntry, ...history]));
+    };
+
+    const handleClearStorage = () => {
+        localStorage.removeItem('history');
+        setHistory([]);
     };
 
     return (
@@ -31,6 +32,9 @@ const App = () => {
             <h1 className={styles.title}>Number Cruncher</h1>
             <InputComponent onSubmit={handleFormSubmit} />
             {history.length > 0 && <TableComponent history={history} />}
+            <button onClick={handleClearStorage} className={styles.clearButton}>
+                Clear History
+            </button>
         </div>
     );
 };
