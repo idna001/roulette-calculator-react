215ab9741ab1d05cd0138b5859aa5a4422545e0b
Andreas V <46223359+idna001@users.noreply.github.com>
2025-01-11 23:48:58 +0100

    enable router and navi

--- src/App.js
+++ src/App.js
@@ -1,103 +1,25 @@
-// App.js
-import React, { useState, useEffect } from 'react';
-import InputComponent from './components/InputComponent/InputComponent';
-import CatComponent from "./components/CatComponent/CatComponent";
-import HistoryComponent from "./components/HistoryComponent/HistoryComponent";
-import {numberArray} from "./data/numberArray";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Cruncher from "./views/cruncher";
+import Cats from "./views/cats";
+
+import './App.css'; // Stellen Sie sicher, dass Sie die CSS-Datei importieren
 
 const App = () => {
-    const [history, setHistory] = useState([]);
-    const [numbers, setNumbers] = useState([]);
-    const [refreshKey, setRefreshKey] = useState(0); // Zustand für den Komponenten-Neu-Render
-
-    useEffect(() => {
-        const storedHistory = JSON.parse(localStorage.getItem('history'));
-        if (storedHistory) {
-            setHistory(storedHistory);
-        }
-    }, [refreshKey]);
-
-    const generateRandomNumber = () => {
-        return Math.floor(Math.random() * 36) + 1;
-    };
-
-    const searchMatchingNumbers = (sum) => {
-        const matchingNumbers = [];
-        for (const key in numberArray) {
-            const [num1, num2] = numberArray[key];
-            if (parseInt(key) === sum) {
-                matchingNumbers.push(num1, num2);
-            }
-        }
-        return matchingNumbers;
-    }
-
-    const handleFormSubmit = (number1, number2) => {
-        const soundEffect = new Audio();
-        soundEffect.autoplay = true;
-        soundEffect.src = '/audio/cat-meow.mp3'
-        soundEffect.play()
-        let sum = parseFloat(number1) + parseFloat(number2);
-        if (sum > 36 || sum < 10) {
-            sum = generateRandomNumber();
-        }
-        const matchingNumbers = searchMatchingNumbers(sum);
-        const thirdNumber = matchingNumbers[0];
-        const fourthNumber = matchingNumbers[1];
-
-        const digitSum = Array.from(sum.toString()).reduce((acc, digit) => acc + parseInt(digit), 0);
-        const matchingNumbersQueer = searchMatchingNumbers(digitSum);
-        const fifth = matchingNumbersQueer[0];
-        const sixth = matchingNumbersQueer[1];
-        const randomNumber = generateRandomNumber();
-        const numbers = [sum, digitSum, thirdNumber, fourthNumber, fifth, sixth, randomNumber];
-        const newEntry = {
-            sum: sum,
-            digitSum: digitSum,
-            numbers: numbers
-        };
-
-        localStorage.setItem('history', JSON.stringify([newEntry]));
-        const existingNumbers = JSON.parse(localStorage.getItem('numbers')) || [];
-        const updatedNumbers = [{ number1, number2 }, ...existingNumbers];
-
-        localStorage.setItem('numbers', JSON.stringify(updatedNumbers));
-        setRefreshKey(prevKey => prevKey + 1);
-    };
-
-    const handleClearStorage = () => {
-        localStorage.removeItem('history');
-        localStorage.removeItem('numbers');
-        setHistory([]);
-        setNumbers([]);
-        setRefreshKey(prevKey => prevKey + 1);
-    };
-    // 3. nachbarzahl
-    // 4. nachbarzahl links
-    // 5. nachbarzahl rechts von der 6
-    // 6. nachbarzahl links von quer summe
-    // 7. zufallszahl zwischen 1 und quer summe
     return (
-        <div className='app'>
-            <img src={require('./img/logo.png')} alt="logo" className='logo' />
-            <h1 className='title'>Lucky cats</h1>
-            <InputComponent onSubmit={handleFormSubmit} />
-{/*
-                <TableComponent history={history} />
-*/}
-            <CatComponent history={history} refreshKey={refreshKey} />
-
-            {history.length > 0 && (
-                <React.Fragment>
-                    <HistoryComponent numbers={numbers} refreshKey={refreshKey} />
-
-                    <button onClick={handleClearStorage} className='clearButton'>
-                        Clear History
-                    </button>
-                </React.Fragment>
-            )}
-
-        </div>
+        <Router>
+            <div className='app'>
+                <Routes>
+                    <Route path="/" element={<Cruncher />} />
+                    <Route path="/cruncher" element={<Cruncher />} />
+                    <Route path="/cats" element={<Cats />} />
+                </Routes>
+                <nav className='bottom-nav'>
+                    <Link to="/cruncher">Cruncher</Link>
+                    <Link to="/cats">Cats</Link>
+                </nav>
+            </div>
+        </Router>
     );
 };
 
