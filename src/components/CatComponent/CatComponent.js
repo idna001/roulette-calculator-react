82c027a4a19822019508595ac0162de7a03f8dce
Andreas V <46223359+idna001@users.noreply.github.com>
2025-01-14 22:43:38 +0100

    reset cats indexes by history delete

--- src/components/CatComponent/CatComponent.js
+++ src/components/CatComponent/CatComponent.js
@@ -5,13 +5,16 @@ const CatComponent = ({ history, refreshKey }) => {
     const [numbers, setNumbers] = useState([]);
     const [isShaking, setIsShaking] = useState(false);
 
+    // src/components/CatComponent/CatComponent.js
     useEffect(() => {
         setIsShaking(true);
         const storedHistory = JSON.parse(localStorage.getItem('catsHistory'));
-        if (storedHistory) {
+        if (storedHistory && storedHistory.length > 0) {
             const lastEntry = storedHistory[0];
             const lastNumbers = lastEntry.numbers;
             setNumbers(lastNumbers);
+        } else {
+            setNumbers([]);
         }
         const timeout = setTimeout(() => {
             setIsShaking(false);
@@ -25,8 +28,12 @@ const CatComponent = ({ history, refreshKey }) => {
         <div className={styles.catOuter}>
             {Array.from({ length: 7 }, (_, index) => (
                 <div className={`${styles.catContainer} ${isShaking ? styles.shake : ''}`} key={index}>
-                    <img src={require('../../img/cat.png')} className={styles.catImage} alt={`Katze ${index + 1}`} />
-                    {numbers.length > 0 && <p className={styles.catNumber}>{numbers[index]}</p>}
+                    <img src={require('../../img/cat.png')}
+                         className={styles.catImage}
+                         alt={`Katze ${index + 1}`} />
+                    {numbers.length > 0 && <p className={styles.catNumber}>
+                        {numbers[index]}
+                    </p>}
                 </div>
             ))}
         </div>
